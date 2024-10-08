8875378060f8481ac5a8111218e0f361892ba1bd
Machaaavynn <160084417+Machaaavynn@users.noreply.github.com>
2024-10-08 12:32:26 +0700

    add database folder

---
+++ lib/database/45r6ty7u.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class vh extends StatelessWidget {
+  const vh({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
